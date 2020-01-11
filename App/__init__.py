0e4b033be8aadd9d6dceb21cf5bbc3fe689cc1af
CRFSlick <47066085+CRFSlick@users.noreply.github.com>
2020-01-10 18:16:42 -0600

    Clean up and fix remote_addr issue when being proxy

--- App/__init__.py
+++ App/__init__.py
@@ -1,25 +1,9 @@
+from werkzeug.contrib.fixers import ProxyFix
 from flask import Flask
-import json
 
 # Initialize the application
 app = Flask(__name__)
-
-# Determine the type of slash this OS uses
-# from App.modules.helper.helper import determine_slash_type
-# slash = determine_slash_type()
-
-
-# Get configuration
-# class Config:
-#     with open(f'{app.root_path}{slash}config{slash}config.json') as config_file:
-#         config = json.load(config_file)
-#
-#     OPENWEATHERMAP_API_KEY = config.get('openweathermap_api_key')
-#     RAPID_API_KEY = config.get('rapid_api_key')
-
-
-# Configure the application with API keys
-# app.config.from_object(Config)
+app.wsgi_app = ProxyFix(app.wsgi_app, num_proxies=1)
 
 # Register application routes
 from App.routes.routes import main
