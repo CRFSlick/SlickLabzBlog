5e54063a453639df8f85e0d865485bebac991ee5
CRFSlick <47066085+CRFSlick@users.noreply.github.com>
2020-02-25 20:32:35 -0600

    Config update

--- App/__init__.py
+++ App/__init__.py
@@ -2,6 +2,7 @@ from werkzeug.contrib.fixers import ProxyFix
 from flask import Flask
 import json
 
+
 # Initialize the application
 app = Flask(__name__)
 app.wsgi_app = ProxyFix(app.wsgi_app, num_proxies=1)
@@ -17,15 +18,21 @@ class Config:
     with open(f'{app.root_path}{slash}config{slash}config.json') as config_file:
         config = json.load(config_file)
 
+    USERNAME = config.get('username')
+    PASSWORD = config.get('password')
+    SECRET_KEY = config.get('secret_key')
     GITHUB_OAUTH = config.get('github_oauth')
 
 
-# Configure the application with API keys
+# Configure the application with config
+app.secret_key = Config.SECRET_KEY
 app.config.from_object(Config)
+
 
 # Register application routes
 from App.routes.routes import main
 app.register_blueprint(main)
+
 
 # Initialize error handler
 from App.routes.error_handler import worthless_var
